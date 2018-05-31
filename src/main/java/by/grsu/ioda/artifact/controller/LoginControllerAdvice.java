5fbfb99d337c4036c6f0cdef6fd894b7fb9ff7bb
AlexandrIoda <ioda_ad_16@mf.grsu.by>
2018-05-31 19:36:00 +0300

    Добавлена авторизация

---
+++ src/main/java/by/grsu/ioda/artifact/controller/LoginControllerAdvice.java
@@ -0,0 +1,33 @@
+package by.grsu.ioda.artifact.controller;
+
+import by.grsu.ioda.artifact.model.User;
+import by.grsu.ioda.artifact.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+public class LoginControllerAdvice {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @ModelAttribute
+    public void addBugetToModel(Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+
+        if (!username.equals("anonymousUser")) {
+            User user = userRepository.findByUsername(username);
+
+            model.addAttribute("account", user);
+            model.addAttribute("authorized", true);
+        } else {
+            model.addAttribute("authorized", false);
+        }
+    }
+
+}
--- src/main/java/by/grsu/ioda/artifact/controller/loginController.java
+++ src/main/java/by/grsu/ioda/artifact/controller/loginController.java
@@ -2,12 +2,14 @@ package by.grsu.ioda.artifact.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/login")
 public class loginController {
+
     @RequestMapping(method = RequestMethod.GET)
     public String login(Model model) {
         return "login";
